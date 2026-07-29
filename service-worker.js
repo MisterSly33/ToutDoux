bf8d5313e52721aeaac511e89f30540db0e5ae24
MisterSly33 <sls@ik.me>
2026-07-29 15:07:09 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,11 @@
-const CACHE = "toutdoux-magali-v1";
+const CACHE = "toutdoux-magali-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
-  );
-});
-
-// Stratégie : réseau d'abord, repli sur le cache si hors-ligne
+self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET") return;
   e.respondWith(
-    fetch(e.request).then((resp) => {
-      const copy = resp.clone();
-      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(()=>{});
-      return resp;
-    }).catch(() => caches.match(e.request).then((r) => r || caches.match("./index.html")))
+    fetch(e.request).then((resp) => { const copy = resp.clone(); caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(()=>{}); return resp; })
+    .catch(() => caches.match(e.request).then((r) => r || caches.match("./index.html")))
   );
 });
