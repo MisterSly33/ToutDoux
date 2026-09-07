81e2bc80b1d9b62e36a1fb086ad86ca8ee9fb730
MisterSly33 <sls@ik.me>
2026-09-07 23:36:03 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "toutdoux-cache-2026-08-28-v33-cyclefix";
+const CACHE_NAME = "toutdoux-cache-2026-08-28-v34-bottles-activities";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
